3672148566ede997bd72e85936153499492d7324
yidasanqian <yidasanqian@gmail.com>
2018-06-22 17:05:44 +0800

    更新sql脚本

--- www.beautyleg7.com/sql/beauty_girl.sql
+++ www.beautyleg7.com/sql/beauty_girl.sql
@@ -1,32 +1,32 @@
 use beauty_girl;
-create table beauty7_album
-(
-	id int unsigned auto_increment
-		primary key,
-	category varchar(45) null,
-	album_url varchar(256) not null comment '专辑链接',
-	album_url_object_id varchar(32) not null comment '专辑链接唯一id',
-	album_title varchar(255) not null comment '专辑链接标题',
-	cover_url varchar(256) null comment '专辑封面链接',
-	number varchar(45) null comment '专辑编号',
-	create_date datetime null comment '创建时间',
-	constraint url_object_id_UNIQUE
-		unique (album_url_object_id)
-) engine=InnoDB;
+CREATE TABLE `beauty7_album` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category` varchar(45) DEFAULT NULL,
+  `album_url` varchar(256) NOT NULL COMMENT '专辑链接',
+  `album_url_object_id` varchar(32) NOT NULL COMMENT '专辑链接唯一id',
+  `album_title` varchar(255) NOT NULL COMMENT '专辑链接标题',
+  `cover_url` varchar(256) DEFAULT NULL COMMENT '专辑封面链接',
+  `number` varchar(45) DEFAULT NULL COMMENT '专辑编号',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url_object_id_UNIQUE` (`album_url_object_id`),
+  KEY `idx_category` (`category`),
+  KEY `idx_create_date` (`create_date`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4
 
-create table beauty7_album_image
-(
-	id int unsigned auto_increment
-		primary key,
-	album_id int(11) unsigned not null comment 'beauty7_album表的主键',
-	item_url varchar(256) not null comment '专辑的内容条目链接',
-	item_url_object_id varchar(32) not null comment '专辑的内容条目链接唯一id',
-	item_url_list_json text null comment '专辑的内容条目链接列表的json形式',
-	stage_name varchar(45) null comment '艺名',
-	publish_date datetime null,
-	item_title varchar(255) null comment '链接内容标题',
-	constraint beauty7__UNIQUE
-		unique (item_url_object_id)
-) engine=InnoDB;
+CREATE TABLE `beauty7_album_image` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `album_id` int(11) unsigned NOT NULL COMMENT 'beauty7_album表的主键',
+  `item_url` varchar(256) NOT NULL COMMENT '专辑的内容条目链接',
+  `item_url_object_id` varchar(32) NOT NULL COMMENT '专辑的内容条目链接唯一id',
+  `item_url_list_json` text COMMENT '专辑的内容条目链接列表的json形式',
+  `stage_name` varchar(45) DEFAULT NULL COMMENT '艺名',
+  `publish_date` datetime DEFAULT NULL,
+  `item_title` varchar(255) DEFAULT NULL COMMENT '链接内容标题',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_item_url_object_id` (`item_url_object_id`),
+  KEY `idx_stage_name` (`stage_name`),
+  KEY `idx_publish_date` (`publish_date`)
+) ENGINE=InnoDB AUTO_INCREMENT=520 DEFAULT CHARSET=utf8mb4
 
 
